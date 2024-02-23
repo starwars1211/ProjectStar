49ae3361794ea9c7b70f82207e3f0959019188ed
starwars1211 <cower1211@gmail.com>
2024-02-23 16:45:29 +0900

    game-1

---
+++ Source/ProjectStar/public/GameMode/EntryHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "EntryHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTSTAR_API AEntryHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
