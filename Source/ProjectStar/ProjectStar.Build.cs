09543e97d4ec252267388241d8737df6f9863009
starwars1211 <cower1211@gmail.com>
2024-02-29 14:33:24 +0900

    1

--- Source/ProjectStar/ProjectStar.Build.cs
+++ Source/ProjectStar/ProjectStar.Build.cs
@@ -10,14 +10,16 @@ public class ProjectStar : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "OnlineSubsystem", "OnlineSubsystemUtils" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        DynamicallyLoadedModuleNames.Add("OnlineSubsystemNull");
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        PublicIncludePaths.AddRange(new string[] { "ProjectStar" });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
--- Source/ProjectStar/public/GameMode/MyGameInstance.h
+++ Source/ProjectStar/public/GameMode/MyGameInstance.h
@@ -14,4 +14,143 @@ class PROJECTSTAR_API UMyGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 	
+public:
+
+	UMyGameInstance(const FObjectInitializer& ObjectInitializer);
+
+public:
+	//---------------------------------[ Create Session ]---------------------------------//
+	/**
+	*	Function to host a game!
+	*
+	*	@Param		UserID			User that started the request
+	*	@Param		SessionName		Name of the Session
+	*	@Param		bIsLAN			Is this is LAN Game?
+	*	@Param		bIsPresence		"Is the Session to create a presence Session"
+	*	@Param		MaxNumPlayers	        Number of Maximum allowed players on this "Session" (Server)
+	*/
+	bool HostSession(TSharedPtr<const FUniqueNetId> UserId, FName SessionName, bool bIsLAN, bool bIsPresence, int32 MaxNumPlayers);
+
+	/* Delegate called when session created */
+	FOnCreateSessionCompleteDelegate OnCreateSessionCompleteDelegate;
+	/* Delegate called when session started */
+	FOnStartSessionCompleteDelegate OnStartSessionCompleteDelegate;
+
+	/** Handles to registered delegates for creating/starting a session */
+	FDelegateHandle OnCreateSessionCompleteDelegateHandle;
+	FDelegateHandle OnStartSessionCompleteDelegateHandle;
+
+	TSharedPtr<class FOnlineSessionSettings> SessionSettings;
+
+	/**
+	*	Function fired when a session create request has completed
+	*
+	*	@param SessionName the name of the session this callback is for
+	*	@param bWasSuccessful true if the async action completed without error, false if there was an error
+	*/
+	virtual void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+
+	/**
+	*	Function fired when a session start request has completed
+	*
+	*	@param SessionName the name of the session this callback is for
+	*	@param bWasSuccessful true if the async action completed without error, false if there was an error
+	*/
+	void OnStartOnlineGameComplete(FName SessionName, bool bWasSuccessful);
+
+	//---------------------------------[ Find Session ]---------------------------------//
+	/**
+	*	Find an online session
+	*
+	*	@param UserId user that initiated the request
+	*	@param bIsLAN are we searching LAN matches
+	*	@param bIsPresence are we searching presence sessions
+	*/
+	void FindSessions(TSharedPtr<const FUniqueNetId> UserId, bool bIsLAN, bool bIsPresence);
+
+	/** Delegate for searching for sessions */
+	FOnFindSessionsCompleteDelegate OnFindSessionsCompleteDelegate;
+
+	/** Handle to registered delegate for searching a session */
+	FDelegateHandle OnFindSessionsCompleteDelegateHandle;
+
+	TSharedPtr<class FOnlineSessionSearch> SessionSearch;
+
+	/**
+	*	Delegate fired when a session search query has completed
+	*
+	*	@param bWasSuccessful true if the async action completed without error, false if there was an error
+	*/
+	void OnFindSessionsComplete(bool bWasSuccessful);
+
+	//---------------------------------[ Join Session ]---------------------------------//
+	/**
+	*	Joins a session via a search result
+	*
+	*	@param SessionName name of session
+	*	@param SearchResult Session to join
+	*
+	*	@return bool true if successful, false otherwise
+	*/
+	bool JoinSession(TSharedPtr<const FUniqueNetId> UserId, FName SessionName, const FOnlineSessionSearchResult& SearchResult);
+
+	/** Delegate for joining a session */
+	FOnJoinSessionCompleteDelegate OnJoinSessionCompleteDelegate;
+
+	/** Handle to registered delegate for joining a session */
+	FDelegateHandle OnJoinSessionCompleteDelegateHandle;
+
+	/**
+	*	Delegate fired when a session join request has completed
+	*
+	*	@param SessionName the name of the session this callback is for
+	*	@param bWasSuccessful true if the async action completed without error, false if there was an error
+	*/
+	void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+
+	//---------------------------------[ Destroy Session ]---------------------------------//
+	/** Delegate for destroying a session */
+	FOnDestroySessionCompleteDelegate OnDestroySessionCompleteDelegate;
+
+	/** Handle to registered delegate for destroying a session */
+	FDelegateHandle OnDestroySessionCompleteDelegateHandle;
+
+	/**
+	*	Delegate fired when a destroying an online session has completed
+	*
+	*	@param SessionName the name of the session this callback is for
+	*	@param bWasSuccessful true if the async action completed without error, false if there was an error
+	*/
+	virtual void OnDestroySessionComplete(FName SessionName, bool bWasSuccessful);
+
+	virtual void Shutdown() override;
+
+	//--------------------------[ Blueprint Call Function ]----------------------------//
+	UFUNCTION(BlueprintCallable, Category = "Network|Test")
+	void StartOnlineGame();
+
+	UFUNCTION(BlueprintCallable, Category = "Network|Test")
+	void FindOnlineGames();
+
+	UFUNCTION(BlueprintCallable, Category = "Network|Test")
+	void JoinOnlineGame(FBlueprintSessionResult SessionResult);
+
+	UFUNCTION(BlueprintCallable, Category = "Network|Test")
+	void DestroySessionAndLeaveGame();
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void OnFindSessionResult(const TArray<FBlueprintSessionResult>& SessionResults);
+
+	void OnFindSessionResult_Implementation(const TArray<FBlueprintSessionResult>& SessionResults);
+
+public:
+	FString m_UserName;
+
+	UFUNCTION(BlueprintCallable)
+	void SetUserName(FString name) { m_UserName = name; };
+
+	UFUNCTION(BlueprintPure)
+	FString GetUserName() { return m_UserName; };
+
 };
